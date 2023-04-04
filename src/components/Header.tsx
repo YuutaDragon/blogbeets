6778596265e5e6cfd7e71c8e9a4e60798231d040
yuutaDragon <guilherme.rodrigues@estudante.ufscar.br>
2023-04-04 19:43:48 -0300

    fix errors

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
 import Image from 'next/image'
 
-import BeetsBlogLogo from '../../public/Beets_Blog_logo.svg' 
+import logo from '../../public/Beets_Blog_logo.svg' 
 
 export default function Header() {
   return (
     <header className='bg-[#55003A] shadow-lg h-20 rounded-md flex items-center px-10'>
-      <Image src={BeetsBlogLogo} width={65} alt="Dois texto, um título com Beets e um subtitulo com Blog" priority={true}/>
+      <Image src={logo} width={65} alt="Dois texto, um título com Beets e um subtitulo com Blog" priority={true}/>
     </header>
   )
 }
