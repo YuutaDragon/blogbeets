703513de6135a60f647652d9cfa7134b7519ebb5
yuutaDragon <guilherme.rodrigues@estudante.ufscar.br>
2023-04-04 19:35:10 -0300

    beets blog

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,28 @@
-import type { AppProps } from 'next/app'
-import { Poppins } from 'next/font/google'
-import { ApolloProvider } from '@apollo/client'
-import { client } from '@/lib/apolo'
+import type { AppProps } from "next/app";
+import { Poppins } from "next/font/google";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "@/lib/apolo";
 
-import Head from 'next/head'
+import Head from "next/head";
 
-import '@/styles/globals.css'
+import "@/styles/globals.css";
 
 const poppins = Poppins({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-  variable: '--font-poppins'
-})
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--font-poppins",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <ApolloProvider client={client}>
-    <Head>
-      <link rel="shortcut icon" href="/favicon.png" type="image/png" />
-      <title>Beets Blog</title>
-    </Head>
-    <main className={`${poppins.variable} font-sans`}>
-      <Component {...pageProps} />
-    </main>
-  </ApolloProvider>
-  )
+    <ApolloProvider client={client}>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+        <title>Beets Blog</title>
+      </Head>
+      <main className={`${poppins.variable} font-sans`}>
+        <Component {...pageProps} />
+      </main>
+    </ApolloProvider>
+  );
 }
